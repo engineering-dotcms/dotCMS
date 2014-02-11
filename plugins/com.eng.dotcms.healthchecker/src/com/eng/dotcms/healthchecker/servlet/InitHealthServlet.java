1cbb6e8f0c67fb61492f12b75db1cd4dd929ff2b
Graziano Aliberti <graziano.aliberti@eng.it>
2014-02-11 10:28:49 +0100

    Health Checker

--- plugins/com.eng.dotcms.healthchecker/src/com/eng/dotcms/healthchecker/servlet/InitHealthServlet.java
+++ plugins/com.eng.dotcms.healthchecker/src/com/eng/dotcms/healthchecker/servlet/InitHealthServlet.java
@@ -6,9 +6,10 @@ import java.net.UnknownHostException;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
-
+import com.dotmarketing.db.HibernateUtil;
 import com.dotmarketing.exception.DotDataException;
 import com.dotmarketing.util.Logger;
+import com.eng.dotcms.healthchecker.AddressStatus;
 import com.eng.dotcms.healthchecker.Operation;
 import com.eng.dotcms.healthchecker.business.HealthCheckerAPI;
 import com.eng.dotcms.healthchecker.util.HealthUtil;
@@ -24,7 +25,11 @@ public class InitHealthServlet extends HttpServlet {
 			String hostname = InetAddress.getLocalHost().getHostName();
 			hostname = HealthUtil.getStringAddress(hostname);
 			Logger.info(getClass(), "STARTING CLUSTER HEALTH INITIALIZATION...");
+			HibernateUtil.startTransaction();
+			// elimino i vecchi records riguardanti il nodo attuale in quanto sono in riavvio.
+			cleanNode(hostname);
 			healthAPI.insertHealthLock(hostname, Operation.STARTING);
+			HibernateUtil.commitTransaction();			
 			Logger.info(getClass(), "...CLUSTER HEALTH INITIALIZATION DONE!");
 		} catch (UnknownHostException e) {
 			Logger.warn(getClass(), "Warning. " + e.getMessage());
@@ -34,5 +39,14 @@ public class InitHealthServlet extends HttpServlet {
 		
 	}
 	
+	private void cleanNode(String localAddress) throws DotDataException {
+		healthAPI.deleteHealthStatus(localAddress, AddressStatus.LEFT);
+		healthAPI.deleteHealthStatus(localAddress, AddressStatus.JOIN);
+		healthAPI.deleteHealthClusterView(localAddress);
+		healthAPI.deleteHealthLock(localAddress, Operation.RESTARTING);
+		healthAPI.deleteHealthLock(localAddress, Operation.FLUSHING);
+		healthAPI.deleteHealthLock(localAddress, Operation.JOINING);
+//		healthAPI.deleteHealthLock(localAddress, Operation.STARTING);
+	}
 	
 }
