8e3a2958bbf6835fe0e1cd1201a9409c62005bb8
Graziano Aliberti <graziano.aliberti@eng.it>
2014-02-12 17:45:01 +0100

    Health Checker

--- plugins/com.eng.dotcms.healthchecker/src/com/eng/dotcms/healthchecker/jdbc/HealthCheckerDAO.java
+++ plugins/com.eng.dotcms.healthchecker/src/com/eng/dotcms/healthchecker/jdbc/HealthCheckerDAO.java
@@ -19,6 +19,8 @@ import static com.eng.dotcms.healthchecker.util.QueryBuilder.ORACLE_SELECT_LOCK;
 import static com.eng.dotcms.healthchecker.util.QueryBuilder.ORACLE_UPDATE_HEALTH_CLUSTER_CREATOR;
 import static com.eng.dotcms.healthchecker.util.QueryBuilder.ORACLE_UPDATE_HEALTH_CLUSTER_VIEW_TIMER;
 
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
